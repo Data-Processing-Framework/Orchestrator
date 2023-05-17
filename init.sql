26a04ce1c62b60aa3edbc7c4d42c79ddca1610d2
Biel Camprubí <bielcamprubi@gmail.com>
2023-05-17 12:28:40 +0200

    Database working!

--- init.sql
+++ init.sql
@@ -1,13 +1,3 @@
-IF NOT EXISTS(SELECT * FROM sys.databases WHERE name = 'logs')
-    BEGIN
-    CREATE DATABASE [logs]
-
-
-    END
-    GO
-        USE [logs]
-    GO
-
 IF NOT EXISTS (SELECT * FROM sysobjects WHERE name='TableName' and xtype='U')
 BEGIN
     CREATE TABLE TableName (
@@ -23,3 +13,12 @@ BEGIN
         [created_by] [char](32) NOT NULL,
     )
 END
+
+CREATE TABLE if not EXISTS [Worker](
+    [id] [bigint] IDENTITY(1,1) NOT NULL,
+    [log_level] [int] NULL,
+    [log_levelname] [char](32) NULL,
+    [log] [char](2048) NOT NULL,
+    [created_at] [datetime2](7) NOT NULL,
+    [created_by] [char](32) NOT NULL,
+)
