d487ae877d35da182d5fb6466f2fe96582791ce5
Biel Camprubí <bielcamprubi@gmail.com>
2023-05-17 18:20:38 +0200

    Implented logging DB

--- init.sql
+++ init.sql
@@ -1,24 +1,11 @@
-IF NOT EXISTS (SELECT * FROM sysobjects WHERE name='TableName' and xtype='U')
-BEGIN
-    CREATE TABLE TableName (
-        Id INT PRIMARY KEY IDENTITY (1, 1),
-        Name VARCHAR(100)
-    )
-    CREATE TABLE [Worker](
-        [id] [bigint] IDENTITY(1,1) NOT NULL,
-        [log_level] [int] NULL,
-        [log_levelname] [char](32) NULL,
-        [log] [char](2048) NOT NULL,
-        [created_at] [datetime2](7) NOT NULL,
-        [created_by] [char](32) NOT NULL,
-    )
-END
-
-CREATE TABLE if not EXISTS [Worker](
-    [id] [bigint] IDENTITY(1,1) NOT NULL,
-    [log_level] [int] NULL,
-    [log_levelname] [char](32) NULL,
-    [log] [char](2048) NOT NULL,
-    [created_at] [datetime2](7) NOT NULL,
-    [created_by] [char](32) NOT NULL,
-)
+CREATE TABLE IF NOT EXISTS `Workers`(
+    `id` BIGINT NOT NULL AUTO_INCREMENT,
+    `log_level` INT NULL,
+    `log_levelname` CHAR(32) NULL,
+    `log` TEXT(2048) NOT NULL,
+    `created_at` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    `created_by` CHAR(32) NOT NULL,
+    `type` CHAR(32) NOT NULL,
+    `name` CHAR(32) NOT NULL,
+    PRIMARY KEY (`id`)
+);
